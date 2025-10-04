920fdc269596895a88ef3bd90e5a88cf34e03fb5
Dominik Zawlocki <zawlockidominik@gmail.com>
2025-10-04 13:41:43 +0200

    Setup DB and get users endpoint

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 
 from flask import Flask
 
+from db_models.Users import User
+from lib import db
+from flask_cors import CORS
+
 from dotenv import load_dotenv
 
 
@@ -11,6 +15,24 @@ def create_app():
     load_dotenv()
     print("Environ:")
     print(os.environ)
+
+    CORS(app, supports_credentials=True, origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ])
+
+    USER = os.getenv("DB_USERNAME")
+    PASSWORD = os.getenv("DB_PASS")
+    HOST = os.getenv("DB_HOST")
+    PORT = os.getenv("DB_PORT")
+    DBNAME = os.getenv("DB_NAME")
+
+    DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     return app
 
@@ -26,5 +48,23 @@ def hello_world():
 @app.route("/test")
 def test_endpoint():
     print("username: ", os.getenv("DB_USERNAME"))
-    return {"test": "endpoint"}
+    return {"test": "endpoint", "user": os.getenv("DB_USERNAME")}
+
+
+@app.route("/users")
+def get_users():
+    print("DB test")
+
+    results = User.query.all()
+
+    print("query results: ", results)
+
+    response = {
+        "users": []
+    }
+
+    for result in results:
+        response["users"].append({"id": result.id, "username": result.username})
+
+    return response
 
---
+++ dbsetup.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+   id SERIAL,
+   username VARCHAR(80),
+   password VARCHAR(80)
+);
+
+
