9a13f7a628d0f4872f5f3941c1e6e2af33e8d546
Dominik Zawlocki <zawlockidominik@gmail.com>
2025-10-04 22:31:30 +0200

    Add build_up_area to Destination

--- db_models/Destination.py
+++ db_models/Destination.py
@@ -12,5 +12,6 @@ class Destination(db.Model):
     historicity = db.Column(db.Float, nullable=False)
     sportiness = db.Column(db.Float, nullable=False)
     forest_cover = db.Column(db.Float, nullable=False)
+    build_up_area = db.Column(db.Float, nullable=False)
     terrain_fluctuation = db.Column(db.Float, nullable=False)
     water = db.Column(db.Float, nullable=False)
