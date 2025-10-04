7b9fc434d27feb2cc32f32d5aeaa9743b0d3ecc2
xXKeNdAmAmAsTeRXx <frkubala@student.agh.edu.pl>
2025-10-04 22:37:41 +0200

    fixes artefact

--- algorithm/alogrithm.py
+++ algorithm/alogrithm.py
@@ -21,21 +21,9 @@ def create_first_time(features_vectors:np.array, chocies:list[bool]) -> np.array
     return softmax(sum)
 
 
-def create_trip(user_preferences: np.array, random: bool = False) -> np.array:
-    num_of_parameters = len(user_preferences)
-
-    if random:
-        return np.random.random(num_of_parameters)
-
-    random_parameter_difference = (np.random.random(num_of_parameters) - 1) * 0.01
-    user_preferences = user_preferences + random_parameter_difference
-    user_preferences = np.clip(user_preferences, 0, 1)
-
-    return user_preferences
-
 def update_preferences(user_preferences: np.array, place_features: np.array, decision: bool) -> dict:
     LR = 0.01
-    FEATURE_NAMES = ["orientality", "temperature", "historicity", "sportiness","forest_cover","terrain_fluctuation", "water"]
+    FEATURE_NAMES = ["orientality", "temperature", "historicity", "sportiness","forest_cover","build_up_area","terrain_fluctuation", "water"]
     RANDOM_PARM_COUNT = 3
     num_features = len(user_preferences)
 
@@ -49,14 +37,11 @@ def update_preferences(user_preferences: np.array, place_features: np.array, dec
     user_preferences = np.clip(user_preferences + LR*direction*(place_features-user_preferences),0,1)
 
     #Chosing Random Fetures for DataBase part
-    indexes = np.random.choice(np.arange(num_features), size=(RANDOM_PARM_COUNT))
-    print(indexes)
+    indexes = np.random.choice(np.arange(num_features), size=(RANDOM_PARM_COUNT), replace=False)
 
     samples = {}
     for i in indexes:
-        samples[FEATURE_NAMES[i]] = user_preferences[i]
+        samples[FEATURE_NAMES[i]] = float(user_preferences[i])
 
     return samples
 
-
-update_preferences(np.array([0,1,2,3,4,5,6,7]), [0,1,2,3,4,5,6,7], 8*[True])
