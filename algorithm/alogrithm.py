16ea9f2aad982f9cd55138182e4374923af338b9
Piotr Matusiewicz <piotrek.matusiewicz@gmail.com>
2025-10-04 18:29:18 +0200

    "Create trip" algorithm

--- algorithm/alogrithm.py
+++ algorithm/alogrithm.py
@@ -5,13 +5,17 @@ def create_first_time():
     return np.zeros(10)
 
 
-
 def create_trip(user_preferences: np.array, random: bool = False) -> np.array:
+    num_of_parameters = len(user_preferences)
+
     if random:
-        return np.random.random(10)
-    #TODO create_trip algo
-    random_vector = np.random.randn(10)
-    return np.zeros(10)
+        return np.random.random(num_of_parameters)
+
+    random_parameter_difference = (np.random.random(num_of_parameters) - 1) * 0.01
+    user_preferences = user_preferences + random_parameter_difference
+    user_preferences = np.clip(user_preferences, 0, 1)
+
+    return user_preferences
 
 def update_preferences(user_preferences: np.array, place_features: np.array, decision: bool) -> np.array:
     learning_rate = 0.01
