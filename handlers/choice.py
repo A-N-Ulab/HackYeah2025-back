2f784ab2d0a2f014b5103502262949b6b62e381e
Piotr Matusiewicz <piotrek.matusiewicz@gmail.com>
2025-10-05 03:23:33 +0200

    Update "choice" template :))

--- handlers/choice.py
+++ handlers/choice.py
@@ -2,9 +2,12 @@ import traceback
 
 from pydantic import BaseModel
 from sqlalchemy.exc import NoReferencedTableError
+from algorithm.alogrithm import create_first_time
 
+from db_models.Destination import Destination
 from db_models.SurveyChoices import SurveyChoices
-from db_models.Trip import Trip, TRIP_STATE_FIRST_SURVEY
+from db_models.Trip import Trip, TRIP_STATE_FIRST_SURVEY, TRIP_STATE_SWAPPING
+
 from stores import MainStore
 
 from lib import db
@@ -40,6 +43,35 @@ def handle(event: Model, store: MainStore):
         traceback.print_exc()
         return {"error": "Invalid ID", "detail": str(err)}, 404
 
-    choicesCount = SurveyChoices.query.filter_by(trip_id=event.trip_id).count()
+    batch_choices = SurveyChoices.query.filter_by(trip_id=event.trip_id).all()
 
-    return {"choice_idx": choicesCount, "total_choices": CHOICES_IN_BATCH}
+    if len(batch_choices) >= CHOICES_IN_BATCH:
+        # prepare for algorithm
+        features_vectors: list[list[float]] = []
+        choices: list[bool] = []
+        for batch_choice in batch_choices:
+            destination  = Destination.query.filter_by(id=batch_choice.destination_id).first()
+
+            features_vectors.append([destination.orientality, destination.temperature, destination.historicity,
+                                     destination.sportiness, destination.forest_cover, destination.build_up_area,
+                                     destination.terrain_fluctuation, destination.water])
+            choices.append(batch_choice.choice)
+
+        algo_solution = create_first_time(features_vectors, choices)
+
+        print(algo_solution)
+
+        # add to trips
+        for solution in algo_solution.keys():
+            setattr(trip, solution, algo_solution[solution])
+        setattr(trip, "state", TRIP_STATE_SWAPPING)
+        # remove from survey
+
+        SurveyChoices.query.filter_by(trip_id=event.trip_id).delete()
+
+        db.session.commit()
+
+
+
+
+    return {"choice_idx": len(batch_choices), "total_choices": CHOICES_IN_BATCH}
